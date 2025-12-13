ca987000e4ee58dd555079d46c46fe3d498db051
DannyValMon <danny00val@gmail.com>
2025-12-12 20:47:00 -0500

    todo

--- 18-mvc/application/model.php
+++ 18-mvc/application/model.php
@@ -7,12 +7,20 @@ class Model
     public function __construct()
     {
         try {
-            $this->db = new PDO("mysql:host=localhost;dbname=pokeadso;charset=utf8", "root", "");
+            $this->db = new PDO(
+                "mysql:host=localhost;dbname=pokeadso;charset=utf8",
+                "root",
+                ""
+            );
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Error DB: " . $e->getMessage());
         }
     }
+
+    // =======================
+    // POKEMONS
+    // =======================
 
     public function listPokemons()
     {
@@ -23,40 +31,68 @@ class Model
 
     public function addPokemon($name, $type, $strength, $stamina, $speed, $accuracy, $trainer_id)
     {
-        $sql = "INSERT INTO pokemons (name, type, strength, stamina, speed, accuracy, trainer_id)
+        $sql = "INSERT INTO pokemons 
+                (name, type, strength, stamina, speed, accuracy, trainer_id)
                 VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         $stmt = $this->db->prepare($sql);
-        return $stmt->execute([$name, $type, $strength, $stamina, $speed, $accuracy, $trainer_id]);
+        return $stmt->execute([
+            $name,
+            $type,
+            $strength,
+            $stamina,
+            $speed,
+            $accuracy,
+            $trainer_id
+        ]);
     }
 
     public function getPokemon($id)
     {
-        $sql = "SELECT * FROM pokemons WHERE id=?";
+        $sql = "SELECT * FROM pokemons WHERE id = ?";
         $stmt = $this->db->prepare($sql);
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function updatePokemon($id, $name, $type, $strength, $stamina, $speed, $accuracy, $trainer_id)
-    {
+    public function updatePokemon(
+        $id,
+        $name,
+        $type,
+        $strength,
+        $stamina,
+        $speed,
+        $accuracy,
+        $trainer_id
+    ) {
         $sql = "UPDATE pokemons 
-                SET name=?, type=?, strength=?, stamina=?, speed=?, accuracy=?, trainer_id=?
-                WHERE id=?";
+                SET name = ?, type = ?, strength = ?, stamina = ?, speed = ?, accuracy = ?, trainer_id = ?
+                WHERE id = ?";
 
         $stmt = $this->db->prepare($sql);
 
         return $stmt->execute([
-            $name, $type, $strength, $stamina, $speed, $accuracy, $trainer_id, $id
+            $name,
+            $type,
+            $strength,
+            $stamina,
+            $speed,
+            $accuracy,
+            $trainer_id,
+            $id
         ]);
     }
 
     public function deletePokemon($id)
     {
-        $sql = "DELETE FROM pokemons WHERE id=?";
+        $sql = "DELETE FROM pokemons WHERE id = ?";
         $stmt = $this->db->prepare($sql);
         return $stmt->execute([$id]);
     }
+
+    // =======================
+    // TRAINERS
+    // =======================
 
     public function listTrainers()
     {
@@ -66,17 +102,14 @@ class Model
     }
 
     public function getPokemonWithTrainer($id)
-{
-    $sql = "SELECT pokemons.*, trainers.name AS trainer_name
-            FROM pokemons
-            LEFT JOIN trainers ON pokemons.trainer_id = trainers.id
-            WHERE pokemons.id = ?";
+    {
+        $sql = "SELECT pokemons.*, trainers.name AS trainer_name
+                FROM pokemons
+                LEFT JOIN trainers ON pokemons.trainer_id = trainers.id
+                WHERE pokemons.id = ?";
 
-    $stmt = $this->db->prepare($sql);
-    $stmt->execute([$id]);
-    return $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
-
-}
-
-    
